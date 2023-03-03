d8fecf8165e3bad21310345cc35ec702606530f3
mbugua <jobugua97@gmail.com>
2023-03-03 20:29:34 +0300

    newer commits

--- Iot_group_work/dht_interface/views.py
+++ Iot_group_work/dht_interface/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from . models import Sensor_values
 
 
 def dht_homepage(request):
-    return render(request,'dht_interface/index.html')
+    data=Sensor_values.objects.all()
+    context={'data':data}
+    return render(request,'dht_interface/index.html',context)
 
 # Create your views here.
