eba6b1fdd0359491bb97f60db7d2979992775610
mbugua <jobugua97@gmail.com>
2023-03-03 21:23:30 +0300

    added the dht sensor

--- Iot_group_work/dht_interface/views.py
+++ Iot_group_work/dht_interface/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from . models import Sensor_values
+import Adafruit_DHT
 
 
+pin = 4
+sensor = Adafruit_DHT.DHT11
 def dht_homepage(request):
+    pin = 4
+    sensor = Adafruit_DHT.DHT11
+    hum, temp = Adafruit_DHT.read_retry(sensor, pin)
+    if hum is not None and temp is not None:
+        print(f'Temperature: {temp}')
+        print(f'Humidity: {hum}')
+    else:
+        print('Failed to read sensor data')
+
     data=Sensor_values.objects.all()
+    rsvd_data=Sensor_values(temprature=temp,humidity=hum)
+    rsvd_data.save()
     context={'data':data}
     return render(request,'dht_interface/index.html',context)
-
-# Create your views here.
