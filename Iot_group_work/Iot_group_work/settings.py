bd451650051478cb1c72e753ff531187a8953853
mbugua <jobugua97@gmail.com>
2023-03-03 16:15:54 +0300

    updated the table on the webpage

--- Iot_group_work/Iot_group_work/settings.py
+++ Iot_group_work/Iot_group_work/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--6m&unxof+_&m*d9rpl#jf3x_^6i+!85=p1bvcp)3h*31heeqj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
