1a679e3756f5272918c58eedcdab0ae2c84c98b5
mbugua <jobugua97@gmail.com>
2023-03-03 16:25:12 +0300

    added the database model

--- Iot_group_work/dht_interface/models.py
+++ Iot_group_work/dht_interface/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Sensor_Data(models.Model):
+    temprature=models.IntegerField()
+    humidity=models.IntegerField()
+
+    def __str__(self):
+        return self.temprature
+
